f641bbe9148e27f1f151200a9262960fc4ed9937
leandroW <leandro_antunes28@hotmail.com>
2024-12-17 17:33:09 -0300

    sistema hospedagem

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,49 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Capacidade menor que o número de hóspedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.1m);
+            }
+            return valor;
+        }
+    }
+}

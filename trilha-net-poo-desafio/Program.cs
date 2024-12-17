2d15896478b3c3b175a845b2905e17924f9845b2
leandroW <leandro_antunes28@hotmail.com>
2024-12-17 17:34:57 -0300

    desafio celulares

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+
+
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
